587614d2aef97a2cec8a3f470def455de0d10ec9
Nabin Chand <nbc0117rdx@gmail.com>
2022-11-22 12:04:00 +0545

    rendering static file in node js

--- app.js
+++ app.js
@@ -4,12 +4,19 @@
 // console.log(add.PI)
 // console.log(new add.SomeObject())
 
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
-app.get('/',(req,res)=>{
-   res.send("Hello world welcome to express js")
-})
-app.listen(3000,()=>{
-    console.log(`server is running in 3000`)
-})
+// app.get('/',(req,res)=>{
+//    res.send("Hello world welcome to express js")
+// })
+// app.listen(3000,()=>{
+//     console.log(`server is running in 3000`)
+// })
+const express = require("express");
+const path = require("path");
+const app =  express();
+app.use(express.static(path.join(__dirname, "/public")));
+app.listen(3000, () => {  
+    console.log("Server running on 3000");
+});
